d6f5610c42393fc30813558d321de81861c55c24
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-22 21:07:17 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ end
 
 def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
   Movie.create do |m|
-    m.create = title:
+    m.title = title
     m.release_date = release_date:
   end
 end
